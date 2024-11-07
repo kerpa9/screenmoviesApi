9ab3930d7cbcba2ae6883f7e30c295c590ec2395
Kevin Reyes <kerpa9@gmail.com>
2024-11-07 14:38:18 -0500

    Create category and data episode

---
+++ src/main/java/com/kevenreyes/searchmovies/models/Category.java
@@ -0,0 +1,24 @@
+package com.kevenreyes.searchmovies.models;
+
+public enum Category {
+    ACTION("Action"),
+    ROMANCE("Romance"),
+    COMEDY("Comedy"),
+    DRAMA("Drama"),
+    CRIME("Crime");
+
+    private String categoryOmdb;
+
+    Category(String categoryOmdb) {
+        this.categoryOmdb = categoryOmdb;
+    }
+
+    public static Category fromString(String text) {
+        for (Category category : Category.values()) {
+            if (category.categoryOmdb.equalsIgnoreCase(text)) {
+                return category;
+            }
+        }
+        throw new IllegalArgumentException("No category found" + text);
+    }
+}
---
+++ src/main/java/com/kevenreyes/searchmovies/models/DataEpisode.java
@@ -0,0 +1,14 @@
+package com.kevenreyes.searchmovies.models;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+public record DataEpisode(
+    @JsonAlias("Title") String title,
+    @JsonAlias("Episode")Integer numEpisode,
+    @JsonAlias("imdbRating")String imdbRating,
+    @JsonAlias("Released")String dateReleased
+){
+
+}
