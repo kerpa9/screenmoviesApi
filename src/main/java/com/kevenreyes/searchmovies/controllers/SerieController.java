5df5a39502d6d1d1da6b843338b59642d580cffc
Kevin Reyes <kerpa9@gmail.com>
2024-11-12 16:45:01 -0500

    Create services and endpoints get and get top five

--- src/main/java/com/kevenreyes/searchmovies/controllers/SerieController.java
+++ src/main/java/com/kevenreyes/searchmovies/controllers/SerieController.java
@@ -1,26 +1,28 @@
 package com.kevenreyes.searchmovies.controllers;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kevenreyes.searchmovies.dto.SerieDTO;
-import com.kevenreyes.searchmovies.repository.SeriesRepository;
+import com.kevenreyes.searchmovies.services.SerieServices;
 
 @RestController
 public class SerieController {
 
     @Autowired
-    private SeriesRepository repository;
+    SerieServices services;
 
-    @GetMapping("/series")
+    @GetMapping("/api/v1/series")
     public List<SerieDTO> allSeries() {
 
-        return repository.findAll().stream().map(s -> new SerieDTO(s.getTitle(), s.getTotalSeasons(), s.getevaluation(),
-                s.getPoster(), s.getGenre(), s.getActors(), s.getSynopsis())).collect(Collectors.toList());
+        return services.allSeries();
     }
 
+    @GetMapping("/api/v1/series/top5")
+    public List<SerieDTO> topFiveSeries(){
+        return services.topFive();
+    }
 }
---
+++ src/main/java/com/kevenreyes/searchmovies/services/SerieServices.java
@@ -0,0 +1,34 @@
+package com.kevenreyes.searchmovies.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kevenreyes.searchmovies.dto.SerieDTO;
+import com.kevenreyes.searchmovies.models.Serie;
+import com.kevenreyes.searchmovies.repository.SeriesRepository;
+
+@Service
+public class SerieServices {
+
+    @Autowired
+    private SeriesRepository repository;
+
+    public List<SerieDTO> allSeries() {
+
+        return repository.findAll().stream().map(s -> new SerieDTO(s.getTitle(), s.getTotalSeasons(), s.getevaluation(),
+                s.getPoster(), s.getGenre(), s.getActors(), s.getSynopsis())).collect(Collectors.toList());
+    }
+
+    public List<SerieDTO> topFive(){
+        return  dataConverter(repository.findTop5ByOrderByEvaluationDesc());
+    }
+
+    public List<SerieDTO> dataConverter(List<Serie> serie){
+        return serie.stream().map(s -> new SerieDTO(s.getTitle(), s.getTotalSeasons(), s.getevaluation(),
+        s.getPoster(), s.getGenre(), s.getActors(), s.getSynopsis())).collect(Collectors.toList());
+    }
+
+}
